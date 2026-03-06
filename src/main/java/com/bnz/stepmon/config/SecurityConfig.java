5003d8bd6e9d615205c3733a06c10c6a83fac7c6
sjo <bonzoman@gmail.com>
2026-03-07 02:18:50 +0900

    로그인2

--- src/main/java/com/bnz/stepmon/config/SecurityConfig.java
+++ src/main/java/com/bnz/stepmon/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // HTTPS 환경에서 POST 요청이 차단되지 않도록 CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/css/**", "/js/**", "/images/**", "/webjars/**", "/favicon.ico").permitAll()
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Swagger 허용
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/health").permitAll() // Swagger 허용
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
                 .formLogin(form -> form
